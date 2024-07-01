ab1fc60ecfe7e8a3f41339fcc1e20a51441679e3
KatiaLosignore <katialos.kl@gmail.com>
2024-07-01 15:39:12 +0200

    PrimoCommitMVC

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyCourse.Controllers
+{
+    // [Route("[controller]")]
+    public class CoursesController : Controller
+    {
+        // private readonly ILogger<CoursesController> _logger;
+
+        // public CoursesController(ILogger<CoursesController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        public IActionResult Index()
+        {
+            return Content("Sono Index");
+        }
+
+         public IActionResult Detail(string id)
+        {
+            return Content($"Sono Detail con id {id}");
+        }
+
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View("Error!");
+        // }
+    }
+}
